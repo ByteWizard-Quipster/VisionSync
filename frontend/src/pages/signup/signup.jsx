56ceb656d30c650537cc53026a546debf8d6bb24
ByteWizard-Quipster <suyd845@gmail.com>
2024-03-20 16:31:11 +0530

    signup ui

--- frontend/src/pages/signup/signup.jsx
+++ frontend/src/pages/signup/signup.jsx
@@ -1,74 +1,106 @@
 //import { useState } from 'react'
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Card from '@mui/material/Card';
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import login_image from "../../assets/login_image.png";
 function Signup() {
   return (
-    <>   <Card variant="outlined"> 
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            margin: 5 ,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
- 
- 
-          <Typography component="h1" variant="h5">
-            Signup
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+    <>
+      {" "}
+      <Card variant="outlined">
+        <Grid container component="main" sx={{ height: "70vh" }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: `url(${login_image})`,
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <Grid
+            item
+            xs={12}
+            sm={8}
+            md={5}
+            component={Paper}
+            elevation={1}
+            square
+          >
+            <Box
+              sx={{
+                margin: 5,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              Create Account
-            </Button>
-            
-                <Link to="/">
-                  {"Already have an account? Sign Up"}
-                </Link>
-          
- 
-          </Box>
-          
-        </Box>
-    
-      </Container>
+              <Typography component="h1" variant="h5">
+                Create a free account
+              </Typography>
+              <Box component="form" noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  CREATE ACCOUNT
+                </Button>
+
+                <Link to="/">{"Already have an account? Sign in"}</Link>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Card>
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
